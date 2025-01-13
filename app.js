5434ca8bdb1b02376691b29c4d7d9d97573be211
erdemaltu <e-altug@hotmail.com>
2025-01-13 23:22:20 +0300

    Categories apis created

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const connectDB = require('./src/config/db');
 const userRoutes = require('./src/routes/userRoutes');
 const contentRoutes = require('./src/routes/contentRoutes');
+const categoryRoutes = require("./src/routes/categoryRoutes");
 
 dotenv.config();
 connectDB();
@@ -17,6 +18,7 @@ app.get('/', (req, res) => {
 });
 app.use('/api/users', userRoutes);
 app.use('/api/contents', contentRoutes);
+app.use("/api/categories", categoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server ${PORT} portunda çalışıyor.`));
